85e77a734cc624e30f1b8eea24483f9e02ff6b49
Dmytro S Lituiev <d.lituiev@gmail.com>
2019-04-24 15:00:57 -0700

    ellipse; self-intersection

--- slideslicer/geom_tools.py
+++ slideslicer/geom_tools.py
@@ -2,21 +2,50 @@ from functools import reduce
 import numpy as np
 import shapely
 from shapely.geometry import Polygon
+from shapely.geometry import MultiLineString
 import cv2
 from warnings import warn
 
 
 def clean_polygon(pp):
+    area = pp.area
     coords = np.asarray(pp.boundary.coords)
-    danglingpiece = np.asarray(pp.buffer(0).boundary.coords)
+    buff = pp.buffer(0)
+    if buff.area ==0:
+        for _ in range(3):
+            pp = _permute_polygon_(pp, fraction=3)
+            buff = pp.buffer(0)
+            if buff.area*3 > area:
+                break
+    boundary = buff.boundary
+    try:
+        if isinstance(boundary, MultiLineString):
+            if len(boundary) == 0:
+                return Polygon()
+            ind = np.argmax([x.area for x in  boundary])
+            print('areas', [x.area for x in  boundary])
+            boundary = boundary[ind]
+        danglingpiece = np.asarray(boundary.coords)
+    except NotImplementedError as ee:
+        warn("shape type:\t{}".format(type(pp)))
+        warn("boundary type:\t{}".format(type(boundary)))
+        raise ee
+        
     negmask = reduce(lambda x,y: x|y, ((coords == row).all(1) for row in danglingpiece))
+    if np.mean(negmask)>0.5:
+        #warn('having to invert the mask')
+        negmask = ~negmask
     pp = Polygon(coords[~negmask])
     return pp.buffer(0)
 
 
 def resolve_selfintersection(pp, areathr=1e-3, fraction=5, depth = 0):
     pbuf = pp.buffer(0)#.interiors
-    areadiff = abs(pp.area- pbuf.area)/pp.area
+    try:
+        areadiff = abs(pp.area- pbuf.area)/pp.area
+    except ZeroDivisionError:
+        return pp
+
         
     if (areadiff > areathr):
         pp = clean_polygon(pp)
@@ -32,6 +61,7 @@ def resolve_selfintersection(pp, areathr=1e-3, fraction=5, depth = 0):
     assert pp.is_valid
     return pp
 
+
 def _permute_vertices_(vertices, fraction=3, break_point=None):
     if break_point is None:
         if fraction>1:
@@ -42,6 +72,7 @@ def _permute_vertices_(vertices, fraction=3, break_point=None):
     pp = Polygon(np.flipud(np.vstack([vertices[break_point:],
                                       vertices[:break_point]])))
     return pp
+
 
 def _permute_polygon_(pp, fraction=3, break_point=None):
     vertices = np.asarray(pp.boundary)
@@ -66,3 +97,28 @@ def get_contour_centre(vertices):
     cX = int(mmnts["m10"] / mmnts["m00"])
     cY = int(mmnts["m01"] / mmnts["m00"])
     return (cX, cY)
+
+
+def get_ellipse_verts_from_bbox(vertices, points=50):
+    (x0,y0), (x1, y1) = vertices
+    MajorAxisLength = int(1*abs(x0-x1))
+    MinorAxisLength = int(1*abs(y0-y1))
+    Centroid = np.r_[(x1+x0)/2, (y1+y0)/2]
+    xx, yy = get_ellipse_verts((MinorAxisLength, MajorAxisLength) , Centroid)
+    return list(zip(xx,yy))
+
+
+def get_ellipse_verts(AxesLengths, Centroid, points=50):
+    """from matlab blog:
+    https://blogs.mathworks.com/steve/2015/08/17/ellipse-visualization-and-regionprops/"""
+    MinorAxisLength, MajorAxisLength = AxesLengths
+    t = np.linspace(0,2*np.pi,points);
+
+    a = MajorAxisLength/2;
+    b = MinorAxisLength/2;
+    Xc = Centroid[0];
+    Yc = Centroid[1];
+    phi = 0# deg2rad(-s(k).Orientation);
+    x = Xc + a*np.cos(t)*np.cos(phi) - b*np.sin(t)*np.sin(phi);
+    y = Yc + a*np.cos(t)*np.sin(phi) + b*np.sin(t)*np.cos(phi);
+    return x,y
